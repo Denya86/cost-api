60e4a8b9c3a285fa1819465c7469ef93ae1f4374
denya86 <denya4444@gmail.com>
2020-09-21 12:38:48 +0300

    create test api

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
 
+Route::apiResource('/cost', 'API\CostController')->middleware('auth:api');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
